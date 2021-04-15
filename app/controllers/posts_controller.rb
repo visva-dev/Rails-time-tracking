31e0fc39e70063a72209e71fb2e99211ffca02ef
Visvaldas <visva.rapalis@gmail.com>
2021-04-15 16:19:41 +0300

    Add styping with Bootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show]
 
-  def index; end
+  def index
+    @posts = Post.all
+  end
 
   def new
     @post = Post.new

93c3538f13328592844bd5e9756516431f6ff80e
Visvaldas <visva.rapalis@gmail.com>
2021-04-15 21:54:17 +0300

    Implemented the ability to delete post records

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[show edit update]
+  before_action :set_post, only: %i[show edit update destroy]
 
   def index
     @posts = Post.all
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
-      redirect_to @post, notice: 'Your post was created successfully'
+      redirect_to @post, notice: 'Your post was edited successfully'
     else
       render :edit
     end
@@ -30,6 +30,11 @@ class PostsController < ApplicationController
   end
 
   def show; end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path, notice: 'Your post was deleted successfully'
+  end
 
   private
 
